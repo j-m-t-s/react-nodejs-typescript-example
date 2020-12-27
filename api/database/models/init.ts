3e6cc5301923e70892a1662e3ef5fe5237e29f19
jackseary <jackmseary@gmail.com>
2020-12-26 21:46:22 -0800

    add sequelize and models and postgres package, update ts-node(think this fixed debugging)

---
+++ api/database/models/init.ts
@@ -0,0 +1,28 @@
+import { DataTypes, Sequelize } from "sequelize/types";
+import { Sneaker } from "./sneaker";
+
+
+export const initModels = (sequelize: Sequelize) => {
+    Sneaker.init({
+        id: {
+          type: DataTypes.INTEGER.UNSIGNED, // you can omit the `new` but this is discouraged
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        name: {
+          type: new DataTypes.STRING(128),
+          allowNull: false,
+        },
+        description: {
+          type: new DataTypes.STRING(128),
+          allowNull: true,
+        },
+        imageUrl: {
+          type: new DataTypes.STRING(128),
+          allowNull: true,
+        }
+      }, {
+        sequelize,
+        tableName: 'sneakers',
+      });
+}
